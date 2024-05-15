6b573285b34c7ba0409e452a847202d6891c647b
kacpergagatek <k.gagatek.21@unimail.winchester.ac.uk>
2024-05-15 23:40:16 +0100

    yes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -41,7 +41,7 @@
 
 
 import React, { useState } from 'react'
-import { Alert, Button, Card } from 'react-bootstrap'
+import { Alert, Button, Card, CardGroup } from 'react-bootstrap'
 
 
 export default function Dashboard() {
@@ -62,20 +62,22 @@ export default function Dashboard() {
 
   return (
     plant.map((variant) => (
-      <Card style={{ width: '18rem' }} 
-      key="dark"
-      className="mb-2"
-      >
-      <Card.Img variant="top" src={require("../images/" + variant.img)} />
-      <Card.Body>
-        <Card.Title>Nice Plant</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      </Card>
+      <CardGroup>
+        <Card style={{ width: '18rem' }} 
+        key="dark"
+        className="mb-2"
+        >
+        <Card.Img variant="top" src={require("../images/" + variant.img)} />
+        <Card.Body>
+          <Card.Title>Nice Plant</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
+      </CardGroup>
     )
   ))
 }
